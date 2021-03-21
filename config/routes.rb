f51f6f27152f429e5a18addeb5335d6e0dffca22
shah-flip <shah.xhtml@yahoo.com>
2021-03-22 02:01:28 +0530

    Migrated Vue to its controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'vue/index'
+  root 'vue#index'
+
   get 'home/index'
 
-  root 'application#index'
-  get '/*path', to: 'application#index', format: false
+  get '/*path',
+      to: 'vue#index',
+      constraints: ->(req) { !(req.fullpath =~ %r{^\/assets\/.*}) }
 
   # root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
