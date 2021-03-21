b383213ccc60052021ae2433ab8d72cbc812b0d1
shah-flip <shah.xhtml@yahoo.com>
2021-03-22 03:39:23 +0530

    turn off automatically generation of SCSS

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module SelfRailsVuejs
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators.stylesheets = false
   end
 end
